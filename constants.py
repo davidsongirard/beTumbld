3c3d3585ba61d60ee3da6cddee02dc602b9b6c4e
Alexander Bliskovsky <alex@bethesdatech.net>
2013-04-06 23:58:50 -0400

    Added constants

---
+++ constants.py
@@ -0,0 +1,14 @@
+IMAGE_SIZE = 128
+IMAGES_HIGH = 5
+IMAGES_WIDE = 5
+
+BLACK = 0, 0, 0
+BLUE = 0, 0, 255
+YELLOW = 255, 255, 0
+RED = 255, 0, 0
+GREEN = 0, 255, 0
+
+D_COLORS = {0: blue,
+            1: yellow,
+            2: red,
+            3: green}
